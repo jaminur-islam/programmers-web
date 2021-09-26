32c07d1c013d417f078c3b2210b1091112aea08b
Jaminur islam <jaminurislam250@gmail.com>
2021-09-26 15:53:27 +0600

    add title

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -4,9 +4,6 @@ import React from 'react';
 // Cart component
 const Cart = (props) => {
   const {img , name } = props.cart
-  const remove = (e) =>{
-     console.log(e.target)
-  }
   return (
     <div className='selected'>
       <img src={img} alt={name} /> 
